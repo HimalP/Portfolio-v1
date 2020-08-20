bea199a2a0a6e628a97e5e8eb429b3ffefd525e7
Himal <HimalHPatel@gmail.com>
2020-08-19 23:56:27 -0400

    update

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = '';
-            }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
-            }
-        })
-        burger.classList.toggle('toggle');
-    })
-}
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+const navBackground = () => {
+  const nav = document.getElementsByTagName("nav")[0];
+
+  window.addEventListener("scroll", () => {
+    this.scrollY > 600
+      ? nav.classList.add("is-solid")
+      : nav.classList.remove("is-solid");
+  });
+};
 
 navSlide();
+navBackground();
